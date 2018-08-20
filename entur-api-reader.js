3bc9dd6990e8c50ace667c7a1c25c182be0b5d45
Ole-Martin Mørk <olemartin@gmail.com>
2018-08-20 16:06:13 +0200

    Restrukturert koden

--- entur-api-reader.js
+++ entur-api-reader.js
@@ -5,36 +5,27 @@ const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async () => {
     const query = `{stopPlace(id: \"NSR:StopPlace:4227\") {id name estimatedCalls(startTime: \"${moment().format()}\", timeRange: 72100, numberOfDepartures: 10) { realtime aimedArrivalTime aimedDepartureTime expectedArrivalTime expectedDepartureTime actualArrivalTime actualDepartureTime date forBoarding forAlighting destinationDisplay { frontText } quay {   id } serviceJourney {   journeyPattern {     line {  id  name  transportMode     }   } }    }}}`;
-    const body = {
-        query: query,
-    };
 
-    console.log("query", JSON.stringify(body));
-    try {
-        const response = await fetch("https://api.entur.org/journeyplanner/2.0/index/graphql", {
-            method: "POST",
-            body: JSON.stringify(body),
-            headers: {
-                "Content-Type": "application/json",
-                "ET-Client-Name": "Serverless workshop @ BEKK",
-                Accept: "application/json",
-            },
-        });
-        const json = await response.json();
-        console.log("response", JSON.stringify(json));
+    const response = await fetch("https://api.entur.org/journeyplanner/2.0/index/graphql", {
+        method: "POST",
+        body: JSON.stringify({ query }),
+        headers: {
+            "Content-Type": "application/json",
+            "ET-Client-Name": "Serverless workshop @ BEKK",
+            Accept: "application/json",
+        },
+    });
+    const json = await response.json();
 
-        await updateDynamoDB(
-            json.data.stopPlace.estimatedCalls.map(call => {
-                return {
-                    expectedDeparture: call.expectedDepartureTime,
-                    line: call.serviceJourney.journeyPattern.line.name,
-                    realtime: call.realtime,
-                };
-            })
-        );
-    } catch (e) {
-        console.log("Error fetching or storing data", e);
-    }
+    await updateDynamoDB(
+        json.data.stopPlace.estimatedCalls.map(call => {
+            return {
+                expectedDeparture: call.expectedDepartureTime,
+                line: call.serviceJourney.journeyPattern.line.name,
+                realtime: call.realtime,
+            };
+        })
+    );
 };
 
 const updateDynamoDB = async expectedDepartureTimes => {
