9518af525a817528de62529b21e1a7b60ba1d0bf
Henrik Wingerei <henrik.wingerei@bekk.no>
2018-08-30 20:38:13 +0200

    Frontend henter data fra API-et (henter nye data hvert 10. sekund). Bedre formatering av tidspunkt.

--- frontend/src/Station.js
+++ frontend/src/Station.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 class Station extends Component {
+
+  formatDepartureTime = expectedDeparture => {
+    const momentDate = moment(expectedDeparture);
+    const duration = moment.duration(momentDate.diff(moment()));
+    const minutes = Math.ceil(duration.asMinutes());
+
+    if (minutes === 0) {
+      return 'nå';
+    } else if (minutes <= 10) {
+      return minutes + ' min';
+    } else {
+      return momentDate.format("HH:mm");
+    }
+  }
+
   render() {
     const departures = this.props.departures.map((dep, index) => {
       const realtime = dep.realtime ? (<i className="realtime" />) : null;
+      const formattedDepartureTime = this.formatDepartureTime(dep.expectedDeparture);
+
       return <div key={index} className="departure">
         <div className="departureTimeContainer">
           {realtime}
-          <span className="departureTime">{dep.expectedDeparture}</span>
+          <span className="departureTime">{formattedDepartureTime}</span>
         </div>
         <span className="departureLine">{dep.line}</span>
       </div>
